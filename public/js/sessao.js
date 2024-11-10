2cda045229b2db88df0600f195d6eb0be74876c6
ni-teixeira <nicoly.teixeira@sptech.school>
2024-11-09 22:17:04 -0300

    funcao do cadastro funcionando na api

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -17,20 +17,4 @@ function limparSessao() {
     window.location = "../logincadastro.html";
 }
 
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
-
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
-
-    var divErrosLogin = document.getElementById("mensagemErroLogin");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
-}
 
