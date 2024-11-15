86d34fb9af0e0e64d02f86e9f54cd00d01d30120
ni-teixeira <nicoly.teixeira@sptech.school>
2024-11-15 17:48:20 -0300

    funcionamento da dash

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -4,14 +4,13 @@ function buscarKPIs(){
     var instrucaoSql = `
     SELECT 
         (SELECT COUNT(*) FROM usuario) as total_usuarios,
-        (SELECT ROUND(AVG(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())), 1) FROM USUARIO) as media_idade,
+        (SELECT ROUND(AVG(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())), 1) FROM usuario) as media_idade,
         (SELECT a.nome FROM album a 
         JOIN usuario u ON a.idAlbum = u.fkAlbum
         GROUP BY a.idAlbum, a.nome
-        ORDER BY COUNT(*) DESC]
+        ORDER BY COUNT(*) DESC
         LIMIT 1) as album_mais_selecionado 
     `;
-
     console.log('Executando a instrução SQL: \n' + instrucaoSql);
     return database.executar(instrucaoSql);
 }
@@ -21,12 +20,12 @@ function buscarUsuariosPorAlbum(){
     SELECT
         a.nome as nome_album,
         COUNT(u.idUsuario) as qtd_usuarios,
-        ROUND((COUNT u.idUsuario) * 100) / (SELECT COUNT(*) FROM usuario), 1) as porcentagem
-        FROM album a
-        LEFT JOIN usuario u ON a.idAlbum = u.fkAlbum
-        GROUP BY a.idAlbum, a.nome
-        ORDER BY qtd_usuarios DESC
-    `
+        ROUND((COUNT(u.idUsuario) * 100) / (SELECT COUNT(*) FROM usuario), 1) as porcentagem
+    FROM album a
+    LEFT JOIN usuario u ON a.idAlbum = u.fkAlbum
+    GROUP BY a.idAlbum, a.nome
+    ORDER BY qtd_usuarios DESC
+    `;
     console.log('Executando a instrução SQL: \n' + instrucaoSql);
     return database.executar(instrucaoSql);
 }
