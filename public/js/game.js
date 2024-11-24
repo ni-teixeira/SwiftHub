1fb3c6e66704d82f7530ebe2230ce2f69d451e40
ni-teixeira <nicoly.teixeira@sptech.school>
2024-11-24 15:33:42 -0300

    add novas musicas no game

--- public/js/game.js
+++ public/js/game.js
@@ -1,13 +1,17 @@
 var pontuacaoAtual = 0;
 
-var respostasCorretas = { // objeto do js usado na funcao verificar
+// objeto do js usado na funcao verificar
+var respostasCorretas = { 
     1:'A', 
     2:'D', 
-    3:'B'
+    3:'B',
+    4:'C',
+    5:'A',
+    6:'D'
 }; 
 
 // uma estrutura de dados que permite armazenar 
-// múltiplos valores relacionados em um único lugar, usando pares de chave e valor.
+// multiplos valores relacionados em um unico lugar, usando pares de chave e valor.
 
 // pergunta atual = numero do onclick
 // resposta = a letra do onclick
@@ -32,6 +36,18 @@ function verificarResposta (perguntaAtual, resposta) {
         document.getElementById('p3').style.display = 'block';
     }
 
+    else if(perguntaAtual == 3) {
+        document.getElementById('p4').style.display = 'block';
+    }
+
+    else if(perguntaAtual == 4) {
+        document.getElementById('p5').style.display = 'block';
+    }
+
+    else if(perguntaAtual == 5) {
+        document.getElementById('p6').style.display = 'block';
+    }
+
     // se for a ultima pergunta
     else {
         mostrarResultadoFinal();
@@ -44,7 +60,7 @@ function verificarResposta (perguntaAtual, resposta) {
 
 function mostrarResultadoFinal() {
     var acertos = pontuacaoAtual/100;
-    var erros = 3 - acertos;
+    var erros = 6 - acertos;
     
     var resultado = document.getElementById('resultado');
 
@@ -60,7 +76,7 @@ function mostrarResultadoFinal() {
 function enviarDadosJogo(pontuacao, acertos, erros) {
     var fkUsuario = sessionStorage.ID_USUARIO;
 
-    // Converter fkUsuario para inteiro
+    // converter fkUsuario para inteiro
     fkUsuario = parseInt(fkUsuario);
 
     fetch("/jogo/registrar", {
